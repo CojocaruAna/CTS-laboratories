b8c4083b01dab215de8ea06ed83c5354e89cdf60
CojocaruAna <73517521+CojocaruAna@users.noreply.github.com>
2021-03-09 15:32:17 +0200

    Add files via upload

---
+++ Week3/src/ro/ase/acs/cts/g1092/refactor/exceptions/InvalidAgeException.java
@@ -0,0 +1,5 @@
+package ro.ase.acs.cts.g1092.refactor.exceptions;
+
+public class InvalidAgeException extends Exception {
+
+}
--- Week3/src/ro/ase/acs/cts/g1092/refactor/phase/Product.java
+++ Week3/src/ro/ase/acs/cts/g1092/refactor/phase/Product.java
@@ -1,35 +1,47 @@
 package ro.ase.acs.cts.g1092.refactor.phase;
 
-import java.nio.channels.UnsupportedAddressTypeException;
+import ro.ase.acs.cts.g1092.refactor.exceptions.InvalidAgeExeption;
+import ro.ase.acs.cts.g1092.refactor.exceptions.InvalidPriceExeption;
 
 public class Product {
 	
 	public static final int MAX_AGE_ACCOUNT = 10;
-	public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
+	public static final float MAX_FIDELITY_DISCOUNT = 0.15f ;
 	
-	public float computePriceWithDiscount(ProductType productType, float initialPrice, int accountAge)
+	public static float getDiscountValue(float price, float discount) {
+		return discount*price;
+	}
+	
+	public static float getFinalPriceWithDiscountandFidelity(float price, float discountValue, float fidelityDiscount) {
+		return (price-discountValue) *(1- fidelityDiscount);
+	}
+	
+	private static float getFidelityDiscount(int accountAge) {
+		return (accountAge > MAX_AGE_ACCOUNT) ? (float)MAX_FIDELITY_DISCOUNT : (float)accountAge/100; 
+	}
+	
+	private static float getFinalPrice(float price, float fidelityDiscount, ProductType type) {
+		float discountValue = getDiscountValue(price, type.getDiscount());
+    	float finalPrice = getFinalPriceWithDiscountandFidelity( price,  discountValue,  0);
+    	return finalPrice;
+	}
+	
+	public float calculeazaPriceWithDiscount(ProductType productType, float price, int accountAge) throws InvalidPriceExeption, InvalidAgeExeption
 	  {
+		if(price <= 0 ) {
+			throw new InvalidPriceExeption();
+		}
+		if(accountAge<0) {
+			throw new InvalidAgeExeption();
+		}
 	    float finalPrice = 0;
-	    float fidelityDiscount = (accountAge > MAX_AGE_ACCOUNT) ? MAX_FIDELITY_DISCOUNT : (float)accountAge/100; 
+	    float fidelityDiscount = (productType == ProductType.NEW)? 0: getFidelityDiscount(accountAge);
 	    
+	    float discountValue = 0;
+	   
+	    finalPrice = getFinalPrice( price,  fidelityDiscount,  ProductType.LEGACY);
+	    	
 	    
-	    switch(productType) {
-	    case NEW:
-	    	finalPrice = initialPrice;
-	    	break;
-	    case DISCOUNTED:
-	    	finalPrice = (initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice));
-	    	break;
-	    case LIMITED_STOCK:
-	    	finalPrice = (initialPrice - (ProductType.LIMITED_STOCK.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.LIMITED_STOCK.getDiscount() * initialPrice));
-	    	break;
-	    case LEGACY:
-	    	finalPrice = (initialPrice - (ProductType.LEGACY.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.LEGACY.getDiscount() * initialPrice));
-	    	break;
-	    	default:
-	    		throw new UnsupportedOperationException("the enum not covered");
-	    }
 	    return finalPrice;
 	  }
-	  
 }
