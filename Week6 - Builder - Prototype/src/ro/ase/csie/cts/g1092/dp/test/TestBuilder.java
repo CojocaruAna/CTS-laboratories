aea8713c6c3d105bde57753c3e428a42fac2d0c2
CojocaruAna <73517521+CojocaruAna@users.noreply.github.com>
2021-03-30 15:34:35 +0300

    Final Commit week 6

--- Week6 - Builder - Prototype/src/ro/ase/csie/cts/g1092/dp/test/TestBuilder.java
+++ Week6 - Builder - Prototype/src/ro/ase/csie/cts/g1092/dp/test/TestBuilder.java
@@ -3,6 +3,7 @@ package ro.ase.csie.cts.g1092.dp.test;
 import ro.ase.csie.cts.g1092.dp.builder.Pistol;
 import ro.ase.csie.cts.g1092.dp.builder.SuperHero;
 import ro.ase.csie.cts.g1092.dp.builder.SuperHero.SuperHeroBuilder;
+import ro.ase.csie.cts.g1092.dp.builder.SuperHeroesDirector;
 
 public class TestBuilder {
 
@@ -11,12 +12,12 @@ public class TestBuilder {
 		//create object
 		//SuperHero superHero = new SuperHero();
 		//then initialize the object: do NOT forget
-		//superHero.name = "Superman";
+		//superHero.name = "SuperMan";
 		//superHero.lifePoints = 100;
 		//superHero.superPower = "Can fly";
 		//superHero.superPower = "Laser eyes";
 		//2 approach-constructor
-		//SuperHero superHero = new SuperHero("Superman",100,false,false,null,new Pistol(),"Laser eyes",null);
+		//SuperHero superHero = new SuperHero("SuperMan",100,false,false,null,new Pistol(),"Laser eyes",null);
 		//3 approach-builder-most common in java and .Net
 		//builder is the optimal solution
 		SuperHero superman = new SuperHero.SuperHeroBuilder("Superman",100)
@@ -24,8 +25,15 @@ public class TestBuilder {
 				.addLeftWeapon(new Pistol())
 				.build();
 		
-		SuperHero joker = new SuperHero.SuperHeroBuilder("Joker", 200).isVillain().addLeftWeapon(new Pistol()).addRightWeapon(new Pistol()).build();
+		SuperHero joker = new SuperHero.SuperHeroBuilder("Joker", 200)
+				.isVillain()
+				.addLeftWeapon(new Pistol())
+				.addRightWeapon(new Pistol())
+				.build();
 		
 	}
+	//director stuff
+	SuperHeroesDirector catalogue = new SuperHeroesDirector(new SuperHeroBuilder("Joker",200));
+	SuperHero jokerClone  = catalogue.buildJoker();
 
 }
